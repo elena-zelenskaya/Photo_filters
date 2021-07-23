4ff812d0b05343c31f4153e2941a408c1ebc6f1e
elena-zelenskaya <elena.a.zelenskaya@gmail.com>
2021-07-22 22:49:08 -0700

    limit filter appliance - one time to image

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#Photo filters
+# Photo filters
 
 Allows the user to upload an image and apply different filters to it.
--- scripts.js
+++ scripts.js
@@ -1,31 +1,34 @@
-var file_name = null;
+var input = null;
 var original_image = null;
 var blur_image = null;
 var rainbow_image = null;
 var gray_image = null;
 var red_image = null;
 var canvas = null;
+var first_red_filter = true;
+var first_gray_filter = true;
 
 function upload() {
-	file_name = document.getElementById("finput");
-	original_image = new SimpleImage(file_name);
-	red_image = new SimpleImage(file_name);
-	gray_image = new SimpleImage(file_name);
-	rainbow_image = new SimpleImage(file_name);
-	blur_image = new SimpleImage(file_name);
+	input = document.getElementById("finput");
+	original_image = new SimpleImage(input);
+	red_image = new SimpleImage(input);
+	gray_image = new SimpleImage(input);
+	rainbow_image = new SimpleImage(input);
+	blur_image = new SimpleImage(input);
 	canvas = document.getElementById("canvas");
 	original_image.drawTo(canvas);
 }
 
 function makeGray() {
-	if (isLoaded(gray_image)) {
+	if (isLoaded(gray_image) && first_gray_filter) {
 		filterGray();
 		gray_image.drawTo(canvas);
+		return_to_original(gray_image);
 	}
 }
 
 function makeRed() {
-	if (isLoaded(red_image)) {
+	if (isLoaded(red_image) && first_red_filter) {
 		filterRed();
 		red_image.drawTo(canvas);
 	}
@@ -38,22 +41,23 @@ function filterGray() {
 		pixel.setGreen(avg);
 		pixel.setBlue(avg);
 	}
+	first_gray_filter = false;
 }
 
 function filterRed() {
 	for (var pixel of red_image.values()) {
 		var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
 		if (avg < 128) {
-			pixel.setRed(2*avg);
+			pixel.setRed(2 * avg);
 			pixel.setGreen(0);
 			pixel.setBlue(0);
-		}
-		else {
+		} else {
 			pixel.setRed(255);
-			pixel.setGreen(2*avg-255);
-			pixel.setBlue(2*avg-255);
+			pixel.setGreen(2 * avg - 255);
+			pixel.setBlue(2 * avg - 255);
 		}
 	}
+	first_red_filter = false;
 }
 
 function makeRainbow() {}
@@ -63,17 +67,26 @@ function makeBlur() {}
 function isLoaded(image) {
 	if (image != null && image.complete()) {
 		return true;
+	} else {
+		alert("Image is not loaded");
+		return false;
 	}
-	alert("Image is not loaded");
-	return false;
+}
+
+function return_to_original(image) {
+	if (isLoaded(image)) {
+		image = new SimpleImage(input);
+	}
 }
 
 function reset() {
 	if (isLoaded(original_image)) {
 		original_image.drawTo(canvas);
-		red_image = new SimpleImage(file_name);
-		gray_image = new SimpleImage(file_name);
-		rainbow_image = new SimpleImage(file_name);
-		blur_image = new SimpleImage(file_name);
+		red_image = new SimpleImage(input);
+		gray_image = new SimpleImage(input);
+		blur_image = new SimpleImage(input);
+		rainbow_image = new SimpleImage(input);
+		first_red_filter = true;
+		first_gray_filter = true;
 	}
 }
